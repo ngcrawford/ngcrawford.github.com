7ce8d4bc5d69005a09ef3174c0ad10a6afb6f3bd
Nick Crawford <ngcrawford@gmail.com>
2013-01-31 15:36:17 -0500

    Fixed indentation and updated gimli reminder

--- md2txt_csv.py
+++ md2txt_csv.py
@@ -15,37 +15,40 @@ fin = open(fin,'rU')
 header = False
 for count, line in enumerate(fin):
 
-	# Skip header
-	line = line.strip("\n")
-	if line == "---" and header == False:
-		header = True
-		continue
+    # Skip header
+    line = line.strip("\n")
+    if line == "---" and header == False:
+        header = True
+        continue
 
-	if line == "---" and header == True:
-		header = False
-		continue
+    if line == "---" and header == True:
+        header = False
+        continue
 
-	if header == True:
-		continue
+    if header == True:
+        continue
 
-	# All sorts of REGEX fun!
-	line = re.sub("^#+\s", "", line)
-	line = re.sub("[*]*", "", line)
-	line = re.sub("^- ",'  ', line)
-	line = re.sub("^\s*- ", "     ", line)
-	line = re.sub("^   ", "    ", line)
-	if re.search("\[.\]$",line) != None:
-		line = re.sub("\[.\]$","", line)
-		line = re.sub("\[","", line)
-		line = re.sub("\]","", line)
+    # All sorts of REGEX fun!
+    line = re.sub("^#+\s", "", line)
+    line = re.sub("[*]*", "", line)
+    line = re.sub("^- ", '  ', line)
+    line = re.sub("^\s*- ", "     ", line)
+    line = re.sub("^   ", "    ", line)
+    
+    if re.search("\[.\]$", line) != None:
+        line = re.sub("\[.\]$", "", line)
+        line = re.sub("\[", "", line)
+        line = re.sub("\]", "", line)
 
-	if line.startswith('[') == True:
-		continue
+    if line.startswith('[') == True:
+        continue
 
-	print line
+    print line
 
 """
-subproces
+subprocess
 gimli -y -f  index.md -o assets/downloads/
+mv assets/downloads/index.pdf assets/downloads/NGCrawford_CV.pdf
+
 """
 
